596b6b80ee183d71ef56ba2bc8eb9418cc155ff6
Xavier <xavier2696@gmail.com>
2019-06-21 06:38:07 +0800

    public playlists

---
+++ app/services/get_public_playlists.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'http'
+
+# Returns all playlists belonging to an account
+class GetPublicPlaylists
+  def initialize(config)
+    @config = config
+  end
+
+  def call(current_account)
+    response = HTTP.auth("Bearer #{current_account.auth_token}")
+                   .get("#{@config.API_URL}/public")
+
+    response.code == 200 ? response.parse['data'] : nil
+  end
+end
